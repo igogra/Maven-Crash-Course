5c9e60bb9b1931d5627d32f6cc05f93c4de50dbf
Iván Gómez <ivan.gomez.grande@gmail.com>
2017-02-23 14:53:19 +0100

    First commit

---
+++ productservices/src/main/java/com/igogra/product/product/App.java
@@ -0,0 +1,13 @@
+package com.igogra.product.product;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

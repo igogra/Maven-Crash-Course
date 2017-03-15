b2305fb159fc08e73a5e40e7dd867ff364106c55
Iván Gómez <ivan.gomez.grande@gmail.com>
2017-03-15 14:27:04 +0100

    Create Product Servlet - Invoke the BO method. Code the display Product Servlet. Configure the Spring listener in web.xml

--- productweb/src/main/java/com/igogra/product/servlets/CreateProductServlet.java
+++ productweb/src/main/java/com/igogra/product/servlets/CreateProductServlet.java
@@ -1,11 +1,18 @@
 package com.igogra.product.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.igogra.product.bo.ProductBO;
+import com.igogra.product.dto.Product;
 
 /**
  * Servlet implementation class CreateProductServlet
@@ -15,6 +22,22 @@ public class CreateProductServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		Integer id = Integer.parseInt(request.getParameter("id"));
+		String name = request.getParameter("name");
+		String description = request.getParameter("description");
+		Integer price = Integer.parseInt(request.getParameter("price"));
+
+		Product product = new Product();
+		product.setId(id);
+		product.setName(name);
+		product.setDescription(description);
+		product.setPrice(price);
+
+		WebApplicationContext context = WebApplicationContextUtils.getRequiredWebApplicationContext(this.getServletContext());
+		ProductBO bo = (ProductBO) context.getBean("productbo");
+		bo.create(product);
+
+		PrintWriter out = response.getWriter();
+		out.print("Product created!");
 	}
 }
--- productweb/src/main/java/com/igogra/product/servlets/DisplayProductDetailsServlet.java
+++ productweb/src/main/java/com/igogra/product/servlets/DisplayProductDetailsServlet.java
@@ -1,10 +1,18 @@
 package com.igogra.product.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.igogra.product.bo.ProductBO;
+import com.igogra.product.dto.Product;
 
 /**
  * Servlet implementation class DisplayProductDetailsServlet
@@ -14,6 +22,16 @@ public class DisplayProductDetailsServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		WebApplicationContext context = WebApplicationContextUtils.getRequiredWebApplicationContext(this.getServletContext());
+		ProductBO bo = (ProductBO) context.getBean("productbo");
+		Product product = bo.findProduct(Integer.parseInt(request.getParameter("id")));
+		
+		PrintWriter out = response.getWriter();
+		
+		out.print("<h3>Product details</h3>");
+		out.print("<br>Product ID:" + product.getId());
+		out.print("<br>Product name:" + product.getName());
+		out.print("<br>Product description:" + product.getDescription());
+		out.print("<br>Product price:" + product.getPrice());
 	}
 }
